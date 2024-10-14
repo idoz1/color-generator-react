7740a758ea51ea74e1455f3e69e12b7e6fe8666b
idoz1 <169044973+idoz1@users.noreply.github.com>
2024-10-14 20:14:54 +0300

    finised project

---
+++ src/components/Form.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Form = ({addColor}) => {
+    const [color, setColor] = useState('');
+    return (
+        <section className='container'>
+            <h4>color generator</h4>
+            <form className='color-form' onSubmit={(e) => {
+                e.preventDefault();
+                addColor(color)
+            }}>
+                <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+                <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025'/>
+                <button className='btn' type='submit' style={{background: color}}>submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default Form
